33a9ae55b96c8d1bb2e8536e33b452063cb0177d
Isabel <isamdrituerto@hotmail.com>
2023-02-13 16:46:44 +0100

    Refactored page structure in NavBar, App, and JobContainer

--- voluntech/src/App.js
+++ voluntech/src/App.js
@@ -1,13 +1,48 @@
 import './App.css';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
+import JobContainer from './containers/JobContainer';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./components/About";
+import Partners from "./components/Partners";
+// import Login from "./components/Login";
+// import Login from "./components/Register";
 
 function App() {
+
+  // Create a state for user - starts off as null
+  // Using UseContext, we will pass down "user" 
+
   return (
    <>
+   
+  <BrowserRouter>
+
+    {/* UserContext.provider */}
     <NavBar/>
 
+    <Routes>
+        <Route path= "/about" element =
+        {<About/>}
+        />
+        <Route path= "/jobs" element=
+        {<JobContainer/>}
+        />
+        {/* <Route path="/login" element=
+        {<Login/>}
+        />
+        <Route path="/register" element=
+        {<Register/>}
+        /> */}
+    </Routes>
+
+    <Partners/>
+    
     <Footer/>
+    {/* UserContext.provider */}
+
+  </BrowserRouter>
+    
    </>
   );
 }
--- voluntech/src/components/NavBar.js
+++ voluntech/src/components/NavBar.js
@@ -1,18 +1,22 @@
-import { BrowserRouter, Link, Routes, Route } from "react-router-dom";
-import JobContainer from "../containers/JobContainer";
-import About from "./About";
-import Partners from "./Partners";
+import { Link } from "react-router-dom";
 
 const NavBar = ({}) => {
+
+    // Get user from UseContext
+    // If the user is not null (charity or volunteer), then render the "log out" and "welcome message"
+    // If the user is null, render the "log in" and "sign up" button
+
+    // Swap out main page -- conditional rendering of Login and Register components
+    // Modal -- login and register
+
     return (  
         <>
-        <BrowserRouter>
-                <ul>
+    <ul>
                     <Link to="/">Home</Link>
                     <Link to= "/about">About</Link>
                     <Link to="/jobs">Jobs</Link>
                     <Link to="/partners">Partners</Link>
-                </ul>
+     </ul>
                 
                 <button>
                     <Link to="/GuestLogin">Log in</Link>
@@ -21,19 +25,6 @@ const NavBar = ({}) => {
                 <button>
                     <Link to="/Signup">Sign up</Link>
                 </button>
-
-            <Routes>
-                <Route path= "/about" element =
-                {<About/>}
-                />
-                <Route path= "/jobs" element=
-                {<JobContainer/>}
-                />
-                <Route path = "/partners" element =
-                {<Partners/>}
-                />
-            </Routes>
-        </BrowserRouter>
         </>
     );
 }
