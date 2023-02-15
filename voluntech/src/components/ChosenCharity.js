24879eef755e162ff2ba0ee240922b8e6eb9faf8
Isabel <isamdrituerto@hotmail.com>
2023-02-15 17:18:36 +0000

    Finished functionality for adding list of volunteers to CharityList

--- voluntech/src/components/ChosenCharity.js
+++ voluntech/src/components/ChosenCharity.js
@@ -3,10 +3,6 @@ import { useParams } from "react-router-dom";
 import JobForm from "./JobForm";
 import Job from "./Job";
 
-//map jobs to show all jobs for this particular charity (like how we've done previously)
-//need conditional rendering - (or else will show undefined.name) - because the info you're trying to use in this component does not exist when the page is loaded
-//whenever you have fetch request/asyn, this is when you need to use conditional rendering 
-
 const ChosenCharity = ({charity}) => {
     
     let {id} = useParams();
@@ -33,7 +29,7 @@ const ChosenCharity = ({charity}) => {
     useEffect(() => {
         if (specificCharityJobs.length){
         setChosenCharityJobs(specificCharityJobs.map((job) => {
-            return <Job key={job.id} job={job}/>}
+            return <Job showVolunteers={true} key={job.id} job={job}/>}
        ))}
     }, [specificCharityJobs]);
 
@@ -45,41 +41,6 @@ const ChosenCharity = ({charity}) => {
     <JobForm chosenCharity = {chosenCharity}/>
     <p>Job listings</p>
     {chosenCharityJobs ?  <ul>{chosenCharityJobs}</ul> : <p>No jobs available</p>}
-
-    {chosenCharity.map((job) => {
-        return(
-            <ul>
-                {job.volunteers.map((volunteer) => {
-                    return(
-                        <>
-                        <li key={volunteer.id}>Name: {volunteer.name}</li>
-                        <li key={volunteer.id}>Experience: {volunteer.experience}</li>
-                        </>
-                    )
-                })}
-            </ul>
-    )
-    })}
-
-
-    
-    {/* // {
-    //     cakes.map((cake, index) => {
-    //         return(
-    //             <div key={index}>
-    //                 <h3>{cake.cakeName}</h3>
-    //                 <ul>
-    //                     {cake.ingredients.map((ingredient, index) => {
-    //                         return(
-    //                             <li key={index}>{ingredient}</li>
-    //                         )
-    //                     })}
-    //                 </ul>
-    //             </div>
-    //         )
-    //     })
-    // } */}
-
     </>
     );
 
--- voluntech/src/components/JobList.js
+++ voluntech/src/components/JobList.js
@@ -3,12 +3,12 @@ import Job from "./Job";
 const JobList = ({jobs}) => {
     
     const jobComponents = jobs.map(job => {
-     return <Job key={job.id} job={job} />}
+        return <Job key={job.id} job={job} />}
     )
     
     return ( 
         <>
-    
+
         <div className="grid md:grid-cols-3 gap-1 px-2 ">
             {jobComponents}
         </div>
