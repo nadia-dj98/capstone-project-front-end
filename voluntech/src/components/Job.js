529650f0aabcba443ee278f868a1ab833e33314b
Nadia <nadia_dj@hotmail.co.uk>
2023-02-16 12:43:23 +0000

    added the duration tag and created method to work it out

--- voluntech/src/components/Job.js
+++ voluntech/src/components/Job.js
@@ -1,5 +1,10 @@
 const Job = ({job, showVolunteers}) => {
 
+    //working out the months between start and end date
+    const startDate = new Date(job.startDate);
+    const endDate = new Date(job.endDate);
+    const monthsDiff = (endDate.getFullYear() - startDate.getFullYear()) * 12 + (endDate.getMonth() - startDate.getMonth());
+
     const applyToJob = () => {
         const applicationBody = {
             jobId: job.id, 
@@ -19,13 +24,12 @@ const Job = ({job, showVolunteers}) => {
                 <p className = "text-lg font-bold">{job.role}</p>
                 {job.charity ? <p className="font-bold">{job.charity.name}</p> : ""}
                 <p>{job.description}</p>
-                <p>Start date: {job.startDate} </p>
-                <p>End date: {job.endDate}</p>
-                {/* <p>Volunteers:{job.volunteers}</p> */}
-
+                
                 <span className="bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">{job.location}</span> 
                 {job.charity ? <span className="bg-purple-100 text-purple-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-purple-900 dark:text-purple-300 ">{job.charity.charityCause}</span>  : "" }
                 
+                {monthsDiff > 1 ? <span class="bg-pink-100 text-pink-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-pink-900 dark:text-pink-300">{monthsDiff} months</span> : <span class="bg-pink-100 text-pink-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-pink-900 dark:text-pink-300">{monthsDiff} month</span>  }
+
                 <div class="flex mt-4 space-x-3 md:mt-6">
                 <form className="text-center ">
                     <button onClick={applyToJob}>Apply</button>
