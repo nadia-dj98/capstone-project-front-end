67d3254a56a25149d8cc7e27f9c89b74fcc0305c
Nadia <nadia_dj@hotmail.co.uk>
2023-02-16 15:25:54 +0000

    removed unused code

--- voluntech/src/components/Footer.js
+++ voluntech/src/components/Footer.js
@@ -17,7 +17,7 @@ const Footer = () => {
             
         </div>
     </div>
-    <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+    <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700" />
     <div class="sm:flex sm:items-center sm:justify-between">
         <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">VOLUNTECH™</a>. All Rights Reserved.
         </span>
